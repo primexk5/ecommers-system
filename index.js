5b060fb1899e1de163a8dadf09ce46d00fb6aa84
Barnabas Lapshak <lapshakbarnabas4@gmail.com>
2025-09-08 09:32:36 +0100

    improvement

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const userSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   username: Joi.string().alphanum().min(3).max(30).required().trim(),
   email: Joi.string().email({ tlds: { allow: ['com', 'org', 'ng'] } }).required().trim(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(4).max(6).required(),
 });
 
 
